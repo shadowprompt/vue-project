45d4e8bbc0ea3b6c6e5d767b50d81ea27ce324e8
shadowprompt <huixiong.cn@gmail.com>
2025-06-07 18:54:29 +0800

    尝试分modules

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,30 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import {createHtmlPlugin} from 'vite-plugin-html'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+const filePath = fileURLToPath(new URL('./src/modules/water/index.html', import.meta.url));
+console.log('xxx ~ ', filePath, import.meta.url)
+
+const htmlParams = {
+  minify: true,
+  pages: [
+    {
+      filename: 'index',     // filename 默认是template文件名，就是index.html
+      entry: './main.js',
+      template: './index.html',
+    }
+  ]
+}
+
 // https://vite.dev/config/
 export default defineConfig({
+  root: `./src/modules/water/`,
   plugins: [
     vue(),
+    // createHtmlPlugin(htmlParams),
     vueJsx(),
     vueDevTools(),
   ],
@@ -17,4 +33,17 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  build: {
+    outDir: '../../../dist',
+    rollupOptions: {
+      input: {
+        water: filePath,
+      },
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]',
+      },
+    },
+  }
 })
